976645e18e45a464a77286b5b6a4d23b986646d6
guanwei0711 <guanwei0711@gmail.com>
2026-07-25 16:22:51 +0800

    add prefetch

--- gemm_wmma/main.cu
+++ gemm_wmma/main.cu
@@ -13,6 +13,7 @@
 #include "v5_gemm_load_matrix_double_buffer.cu"
 #include "v6_gemm_gmem_double_buffer.cu"
 #include "v7_gemm_gmem_pipelining.cu"
+#include "v8_gemm_m128n128k16_prefetch.cu"
 
 void gemm_cpu(const std::vector<float>& A, const std::vector<float>& B, std::vector<float>& C,
               int M, int K, int N, float alpha, float beta) {
@@ -165,6 +166,20 @@ int main(int argc, char** argv) {
                     (M + v7_dims::M_SMEM_ROWS - 1) / v7_dims::M_SMEM_ROWS);
         cudaMemcpy(dC, hC.data(), sizeof(half) * sizeC, cudaMemcpyHostToDevice);
         v7_gemm_gmem_pipelining<<<blocks, threads>>>(dA, dB, dC, M, N, K, alpha, beta);
+        cudaDeviceSynchronize();
+        if (check_correctness) {
+            cudaMemcpy(hC_out.data(), dC, sizeof(half) * sizeC, cudaMemcpyDeviceToHost);
+            to_float(hC_out, hC_out_f);
+            printf("v7_gemm_gmem_pipelining max abs error: %e\n", max_abs_error(hC_cpu, hC_out_f));
+        }
+    }
+
+    {
+        dim3 threads(v8_dims::WARP_SIZE * v8_dims::WARPS, 1);
+        dim3 blocks((N + v8_dims::N_SMEM_COLS - 1) / v8_dims::N_SMEM_COLS,
+                    (M + v8_dims::M_SMEM_ROWS - 1) / v8_dims::M_SMEM_ROWS);
+        cudaMemcpy(dC, hC.data(), sizeof(half) * sizeC, cudaMemcpyHostToDevice);
+        v8_gemm_m128n128k16_prefetch<<<blocks, threads>>>(dA, dB, dC, M, N, K, alpha, beta);
         cudaDeviceSynchronize();
         if (check_correctness) {
             cudaMemcpy(hC_out.data(), dC, sizeof(half) * sizeC, cudaMemcpyDeviceToHost);
