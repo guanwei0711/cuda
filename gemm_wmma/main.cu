42cfd4c2d0855933337b37539cd43171386e6bf7
guanwei0711 <guanwei0711@gmail.com>
2026-07-25 17:21:58 +0800

    vectorization

--- gemm_wmma/main.cu
+++ gemm_wmma/main.cu
@@ -15,6 +15,7 @@
 #include "v7_gemm_gmem_pipelining.cu"
 #include "v8_gemm_m128n128k16_prefetch.cu"
 #include "v9_gemm_m128n128k16_gmem_double_buffer.cu"
+#include "v10_gemm_vectorization.cu"
 
 void gemm_cpu(const std::vector<float>& A, const std::vector<float>& B, std::vector<float>& C,
               int M, int K, int N, float alpha, float beta) {
@@ -202,7 +203,20 @@ int main(int argc, char** argv) {
             printf("v9_gemm_m128n128k16_gmem_double_buffer max abs error: %e\n", max_abs_error(hC_cpu, hC_out_f));
         }
     }
-
+    
+    {
+        dim3 threads(v10_dims::WARP_SIZE * v10_dims::WARPS, 1);
+        dim3 blocks((N + v10_dims::N_SMEM_COLS - 1) / v10_dims::N_SMEM_COLS,
+                    (M + v10_dims::M_SMEM_ROWS - 1) / v10_dims::M_SMEM_ROWS);
+        cudaMemcpy(dC, hC.data(), sizeof(half) * sizeC, cudaMemcpyHostToDevice);
+        v10_gemm_vectorization<<<blocks, threads>>>(dA, dB, dC, M, N, K, alpha, beta);
+        cudaDeviceSynchronize();
+        if (check_correctness) {
+            cudaMemcpy(hC_out.data(), dC, sizeof(half) * sizeC, cudaMemcpyDeviceToHost);
+            to_float(hC_out, hC_out_f);
+            printf("v10_gemm_vectorization max abs error: %e\n", max_abs_error(hC_cpu, hC_out_f));
+        }
+    }
     if (RUN_CUBLAS) {
         cublasHandle_t handle = nullptr;
         cublasCreate(&handle);
