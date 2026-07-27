616c13e3b52690273cadc9372535e8266b3d733e
guanwei0711 <guanwei0711@gmail.com>
2026-07-28 03:20:00 +0800

    tidy up

--- gemm/main.cu
+++ gemm/main.cu
@@ -140,22 +140,6 @@ int main(int argc, char** argv) {
     }
 
     {
-        constexpr int Bm = 64, Bn = 64, Bk = 16;
-        constexpr int Tm = 4, Tn = 4;
-        constexpr int THREADS = 256;
-        dim3 threads(THREADS);
-        dim3 blocks((N + Bn - 1) / Bn, (M + Bm - 1) / Bm);
-        cudaMemcpy(dC, hC.data(), sizeof(float) * sizeC, cudaMemcpyHostToDevice);
-        v4_gemm_2d_tiling_conflict_free<Bm, Bn, Bk, Tm, Tn, THREADS><<<blocks, threads>>>(dA, dB, dC, M, K, N, alpha, beta);
-        cudaDeviceSynchronize();
-        if (check_correctness) {
-            cudaMemcpy(hC_kernel.data(), dC, sizeof(float) * sizeC, cudaMemcpyDeviceToHost);
-            float err = max_abs_error(hC_cpu, hC_kernel);
-            printf("v4_gemm_2d_tiling_conflict_free kernel max relative error: %e\n", err);
-        }
-    }
-
-    {
         // this is best accross all dim, with largest possible Bk which reduce number of iterations
         // and balance number of ldg for choosing Bm = 64
         // Also with Tm * Tn = 64, there is more arithmetic operation to hide latency
